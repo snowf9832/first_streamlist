238b757b3d22e850eef00da9b0a8dfee82f4a67f
snowf9832 <120445087+snowf9832@users.noreply.github.com>
2022-12-13 07:36:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title('Snowflake badge 2')
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list('Fruit'))
 
 streamlit.dataframe(my_fruit_list)
