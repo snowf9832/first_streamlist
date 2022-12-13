dc320314727ed925f80b1a2b8effdda7ee14fd2a
snowf9832 <120445087+snowf9832@users.noreply.github.com>
2022-12-13 07:34:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
+import pandas
 
 streamlit.title('Snowflake badge 2')
+
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+streamlit.dataframe(my_fruit_list)
